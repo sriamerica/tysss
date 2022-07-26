e99df4d78631f20e30850117c4c067f8ccdb31ae
sriamerica <srimanthrao1886@gmail.com>
2022-07-26 15:02:28 +0530

    dlete line

--- indiaProjecttyss/src/test/java/indiaProjecttyss1/India1Test.java
+++ indiaProjecttyss/src/test/java/indiaProjecttyss1/India1Test.java
@@ -1,14 +1,11 @@
 package indiaProjecttyss1;
 
-public class India1 
+public class India1Test
 {
 	public static void main(String[] args)
 	{
 		System.out.println("Hi india1");
-		System.out.println("Hi india");
-		//hibsjbjsdjs
-		//hi india
-		//hi modi
-		// hi tiger
+		
+		
 	}
 }
---
+++ indiaProjecttyss/src/test/java/indiaProjecttyss1/JaiHind.java
@@ -0,0 +1,9 @@
+package indiaProjecttyss1;
+
+public class JaiHind 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("jaii hind");	
+	}
+}
