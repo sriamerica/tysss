fb244fc068d6a62b5de9b20a86100c50f817c2d5
sriamerica <109575623+sriamerica@users.noreply.github.com>
2022-07-26 15:06:54 +0530

    Update India1Test.java

--- indiaProjecttyss/src/test/java/indiaProjecttyss1/India1Test.java
+++ indiaProjecttyss/src/test/java/indiaProjecttyss1/India1Test.java
@@ -5,6 +5,10 @@ public class India1Test
 	public static void main(String[] args)
 	{
 		System.out.println("Hi india1");
+		System.out.println("Hi india1");
+		System.out.println("Hi india1");
+		System.out.println("Hi india1");
+		
 		
 		
 	}
